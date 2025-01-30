bbdefdee3c9ec6b402ca1c80e6c20f976f5d099b
artiechokeheart <artemiswilkinson@hotmail.com>
2025-01-30 10:52:05 +0000

    Avoiding merge conflicts

--- utils/checkCategoryExists.js
+++ utils/checkCategoryExists.js
@@ -15,3 +15,18 @@ exports.checkArticleExists = async (article_id) => {
     return Promise.reject({ status: 500, error });
   }
 };
+
+exports.checkUserExists = async (article_id) => {
+  try {
+    const resolved = await db.query(
+      "SELECT * FROM articles WHERE article_id = $1",
+      [article_id]
+    );
+    if (resolved.rows.length === 0) {
+      return Promise.reject({ status: 404, error: {} });
+    }
+    return "check complete - category exists";
+  } catch (error) {
+    return Promise.reject({ status: 500, error });
+  }
+};
